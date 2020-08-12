84b266586abf49d728894744341412f5446ae941
Em Urdaneta <em.urdaneta94@gmail.com>
2020-08-12 15:16:36 +0000

    modify the char counter script

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,31 +1,15 @@
 $(document).ready(function() {
 
-  $(".new-tweet form textarea").on("keyup", function(evt) {
-    let charCount = 0;
+  $(".compose-tweet form textarea").on("keyup", function(evt) {
+    let postLength = $(this).val().length;
+    let charactersLeft = 140 - postLength;
     let counter = $(this).siblings().find(".counter");
-  
-    if(evt.keyCode === 8){
-      charCount = parseInt(counter.val()) +  1 ;
-    } else {
-      charCount = parseInt(counter.val()) - 1;
-    }
-   
-    if(charCount <= 0){
+    counter.text(charactersLeft);
+
+    if(postLength > 140){
       counter.addClass('error');
-      counter.val(charCount);
     } else {
       counter.removeClass('error');
-      if(charCount > 140){
-        counter.val(140);
-      } else {
-        counter.val(charCount);
-      }
     }
-
-    //bug when they highlight and delete. the counter doesnot reset
-   
   });
-
-
-  
 });
